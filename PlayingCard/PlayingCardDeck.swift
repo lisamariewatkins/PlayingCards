402d8ff96eb8d6e33fa82f8a613890a07b1f3f76
Lisa Watkins <lisamwatkins@utexas.edu>
2019-02-12 08:48:58 -0600

    added initial playing card and deck functionality

---
+++ PlayingCard/PlayingCardDeck.swift
@@ -0,0 +1,41 @@
+//
+//  PlayingCardDeck.swift
+//  PlayingCard
+//
+//  Created by Lisa Watkins on 2/12/19.
+//  Copyright © 2019 Lisa Watkins. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCardDeck {
+    private(set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if (cards.count > 0) {
+            return cards.remove(at: cards.count.arc4random)
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+    }
+}
